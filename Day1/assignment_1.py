d467762fc5b0b77f3e1c26dd040434f47fb56a6e
ravi-teja162 <54831763+ravi-teja162@users.noreply.github.com>
2025-04-09 21:49:49 +0530

    Update assignment_1.py

--- Day1/assignment_1.py
+++ Day1/assignment_1.py
@@ -2,18 +2,17 @@ D1 = {'ok': 1, 'nok': 2}
 D2 = {'ok': 2, 'new':3 }
 
 #DictComprehension - union
-print("DictComprehension - union")
-union = {}
+union = D1.copy()
 union.update({key:D2[key] for key in D2.keys() if key not in D1.keys()})
-print(union)
+print("Union: ",union)
 
 
 #DictComprehension - intersection
-print({key : D1[key ]for key in D2.keys() if key in D1.keys()})
+print("Intersection: ",{key : D1[key ]for key in D2.keys() if key in D1.keys()})
 
 
 #DictComprehension - subtraction
-print({key : D1[key] for key in D1.keys() if key not in D2.keys()})
+print("Subtraction(D1-D2): ",{key : D1[key] for key in D1.keys() if key not in D2.keys()})
 
 #merge
 merge = D1.copy()
@@ -22,7 +21,4 @@ for key in D2.keys():
         merge[key] += D2[key]
     else:
         merge[key] = D2[key]
-print(merge)
-
-
-
+print("Merge: ",merge)
