9b048443fae5c6a6e65a8b8db71bcb78c59e2195
ravi-teja162 <54831763+ravi-teja162@users.noreply.github.com>
2025-04-09 23:10:20 +0530

    Update file.py

--- Day4/pkg/file.py
+++ Day4/pkg/file.py
@@ -4,21 +4,31 @@ import datetime
 class File:
     def __init__(self,path):
         self.path = path
-        self.folder = glob.glob(path+r"\*")
+        self.folder = glob.glob(path+r"\*") 
         self.sorted_folder = []
+        self.files_time = []
         
     
     def getMaxSizeFile(self, max_files):
-        self.sorted_folder = sorted(self.folder, key = lambda x: os.path.getsize(x))
+        self.sorted_folder = sorted(self.folder, key = lambda x: os.path.getsize(x), reverse = True)
         return self.sorted_folder[:max_files]
         
-    def getLatestFiles(datetime.date(2018,2,1)):
-        pass
+    def getLatestFiles(self, date):
+        for i in self.folder:
+            self.mtime = datetime.datetime.fromtimestamp(os.path.getmtime(i))
+            if date < self.mtime.date():
+                self.files_time.append(i)
+        return self.files_time
+                
+                
+                
+        
+        
         
         
         
     def __str__(self):
-        return f"{self.sorted_folder}
+        return f"{self.sorted_folder}"
         
 
     
